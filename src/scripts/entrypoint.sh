1dd699bf4b7f45aa9b152fc4b380bace081bfe69
Fabio D'Andria <fabio.dandria@laserromae.it>
2020-09-16 15:42:26 +0200

    add su - to entrypoint

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -5,11 +5,9 @@ cat /etc/passwd | grep $UID
 
 find /opt/sonarqube -ls
 
-cd /opt/sonarqube
-
 # changing the amount of RAM to be used for Java to 75% of available mem
 memory=$( echo "($( free -m | grep '^Mem:' | tr -s ' ' | cut -d ' ' -f 2 ) / 1024 * 0.75 * 1024)/1; scale=0" | bc )
 echo "setting RAM to ${memory} mb"
 
 echo "starting sonarqube"
-java -Xms${memory}m -Xmx${memory}m -jar lib/sonar-application-8.4.2.36762.jar -Djava.security.egd=file:/dev/./urandom
+su -l sonar -c "cd /opt/sonarqube; java -Xms${memory}m -Xmx${memory}m -jar lib/sonar-application-8.4.2.36762.jar -Djava.security.egd=file:/dev/./urandom"
