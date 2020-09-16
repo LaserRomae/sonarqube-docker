a1b8d01f4fbd88e1b0bb539eee9c55ac60ea28c9
Fabio D'Andria <fabio.dandria@laserromae.it>
2020-09-16 16:33:54 +0200

    add debug mode

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # changing the amount of RAM to be used for Java to 75% of available mem
 memory=$( echo "($( free -m | grep '^Mem:' | tr -s ' ' | cut -d ' ' -f 2 ) / 1024 * 0.75 * 1024)/1; scale=0" | bc )
 echo "setting RAM to ${memory} mb"
 
-echo "starting sonarqube"
-cd /opt/sonarqube; java -Xms${memory}m -Xmx${memory}m -jar lib/sonar-application-8.4.2.36762.jar -Djava.security.egd=file:/dev/./urandom
+[ -n "${DEBUG} ] && /bin/bash || {
+  echo "starting sonarqube"
+  cd /opt/sonarqube; java -Xms${memory}m -Xmx${memory}m -jar lib/sonar-application-8.4.2.36762.jar -Djava.security.egd=file:/dev/./urandom
+}
